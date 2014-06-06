dfb3873009e2a72ca14d074a0392692e4186a39e
Matt Bone <thatmattbone@gmail.com>
2014-06-06 12:01:50 -0500

    update requirements.

--- pyramid_test_proj/pserve.py
+++ pyramid_test_proj/pserve.py
@@ -1,11 +1,11 @@
 # copy of pserve for simpler integration tests
 # could be a problem when changing pyramid versions
-__requires__ = 'pyramid==1.4'
+__requires__ = 'pyramid==1.5.1'
 import sys
 from pkg_resources import load_entry_point
 
 if __name__ == '__main__':
     sys.exit(
-        load_entry_point('pyramid==1.4', 'console_scripts', 'pserve')()
+        load_entry_point('pyramid==1.5.1', 'console_scripts', 'pserve')()
     )
 
